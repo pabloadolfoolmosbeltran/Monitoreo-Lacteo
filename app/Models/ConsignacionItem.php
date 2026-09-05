f31ff23f8575bd7cbd1b9c6e64d989df64cbad65
Pablo Adolfo Olmos Beltran <pabloadolfoolmosbeltran@gmail.com>
2026-09-04 20:45:23 -0400

    Avance 14: agrega casts y relaciones de item consignado

--- app/Models/ConsignacionItem.php
+++ app/Models/ConsignacionItem.php
@@ -18,3 +18,28 @@ class ConsignacionItem extends Model
         'consignacion_id',
         'presentacion_id',
         'cantidad_recibida',
+        'cantidad_disponible',
+        'precio_productor',
+        'precio_venta',
+    ];
+
+    protected $casts = [
+        'cantidad_recibida' => 'decimal:3',
+        'cantidad_disponible' => 'decimal:3',
+        'precio_productor' => 'decimal:2',
+        'precio_venta' => 'decimal:2',
+    ];
+
+    protected $appends = [
+        'margen_unitario',
+    ];
+
+    public function consignacion(): BelongsTo
+    {
+        return $this->belongsTo(Consignacion::class);
+    }
+
+    public function presentacion(): BelongsTo
+    {
+        return $this->belongsTo(Presentacion::class);
+    }
