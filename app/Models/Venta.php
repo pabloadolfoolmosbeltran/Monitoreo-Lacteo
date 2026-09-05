969ad3d527ea5f7a13856037f17b7f8d8d9052e7
Pablo Adolfo Olmos Beltran <pabloadolfoolmosbeltran@gmail.com>
2026-09-04 20:45:25 -0400

    Avance 18: relaciona ventas con items y usuarios

--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -18,3 +18,23 @@ class Venta extends Model
         'precio_unitario_venta',
         'fecha_venta',
         'observaciones',
+    ];
+
+    protected $casts = [
+        'cantidad_vendida' => 'decimal:3',
+        'precio_unitario_venta' => 'decimal:2',
+        'fecha_venta' => 'datetime',
+    ];
+
+    protected $appends = [
+        'total_venta',
+        'margen_obtenido',
+    ];
+
+    public function consignacionItem(): BelongsTo
+    {
+        return $this->belongsTo(ConsignacionItem::class);
+    }
+
+    public function vendedor(): BelongsTo
+    {
