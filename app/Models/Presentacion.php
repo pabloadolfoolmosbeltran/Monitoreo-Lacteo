f09ce3ad3e23765920ecd6dd2aaf2ac234fbd6e3
Pablo Adolfo Olmos Beltran <pabloadolfoolmosbeltran@gmail.com>
2026-09-04 20:45:34 -0400

    Avance 32: relaciona presentaciones con consignaciones

--- app/Models/Presentacion.php
+++ app/Models/Presentacion.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Presentacion extends Model
 {
@@ -37,5 +38,10 @@ class Presentacion extends Model
     public function producto(): BelongsTo
     {
         return $this->belongsTo(Producto::class);
+    }
+
+    public function consignacionItems(): HasMany
+    {
+        return $this->hasMany(ConsignacionItem::class);
     }
 }
